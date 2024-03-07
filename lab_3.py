a1aae5ae52245ff27a8b2e5b4f7e1d847460b464
Иван Мслеников <bdfy.maslenikov@yandex.ru>
2024-03-07 19:40:38 +0300

    lab 3

--- lab_3.py
+++ lab_3.py
@@ -1,62 +1,111 @@
 import tkinter as tk
 from cmath import cos, sin, pi, sqrt, acos, asin
 
+
 def f_coord(x):
     return (x + 10) * 40
 
+
+def print_dots(x, y):
+    global canvas
+    x1 = x
+    y1 = 800 - y
+    canvas.create_oval(x1 - 3, y1 - 3, x1 + 3, y1 + 3, fill='red')
+
+
 def line(x0, y0, x1, y1):
-    if y0 > y1:
+    global canvas
+    if x0 > x1:
         x0, x1 = x1, x0
         y0, y1 = y1, y0
-    deltax = abs(x1 - x0)
-    deltay = abs(y1 - y0)
-    if deltax > deltay:
-        print('i am here')
-        error = 0
-        deltaerr = (deltay + 1)
-        y = y0
-        diry = y1 - y0
-        if diry > 0:
-            diry = 1
-        if diry < 0:
-            diry = -1
-        if x0 < x1:
-            for x in range(x0, x1):
-                print_pixel(x, y)
-                error += deltaerr
-                if error >= (deltax + 1):
-                    y += diry
-                    error -= (deltax + 1)
-        else:
-            for x in range(x0, x1, -1):
-                print_pixel(x, y)
-                error += deltaerr
-                if error >= (deltax + 1):
-                    y += diry
-                    error -= (deltax + 1)
-    else:
-        error = 0
-        deltaerr = (deltax + 1)
-        x = x0
-        dirx = x1 - x0
-        if dirx > 0:
-            dirx = 1
-        if dirx < 0:
-            dirx = -1
-        if y0 < y1:
-            for y in range(y0, y1):
-                print_pixel(x, y)
-                error += deltaerr
-                if error >= (deltay + 1):
-                    x += dirx
-                    error -= (deltay + 1)
-        else:
-            for y in range(y0, y1, -1):
-                print_pixel(x, y)
-                error += deltaerr
-                if error >= (deltay + 1):
-                    x += dirx
-                    error -= (deltay + 1)
+
+    xx0, yy0, xx1, yy1 = x0, y0, x1, y1
+    canvas.create_line(x0, 800 - (y0), x1, 800 - (y1))
+    x1 -= x0
+    y1 -= y0
+    x0, y0 = 0, 0
+    canvas.create_line(x0 + 400, 800 - (y0 + 400), x1 + 400, 800 - (y1 + 400), fill='green')
+    flag_x, flag_x_y = 0, 0
+    if y1 < 0:
+        flag_x = 1
+        y1 = abs(y1)
+        canvas.create_line(x0 + 400, 800 - (y0 + 400), x1 + 400, 800 - (y1 + 400), fill='red')
+    if y1 > x1:
+        flag_x_y = 1
+        y1, x1 = x1, y1
+        canvas.create_line(x0 + 400, 800 - (y0 + 400), x1 + 400, 800 - (y1 + 400), fill='blue')
+    error = 0
+    dots = []
+    y = 0
+    deltaerr = (y1 + 1)
+    for x in range(x1):
+        if x % 40 == 0:
+            if y % 40 < 40 - y % 40:
+                dots.append([x, y - y % 40])
+            else:
+                dots.append([x, y - y % 40 + 40])
+        error += deltaerr
+        if error >= (x1 + 1):
+            y += 1
+            error -= (x1 + 1)
+    if flag_x_y == 1:
+        for i in dots:
+            i[0], i[1] = i[1], i[0]
+    if flag_x:
+        for i in dots:
+            i[1] = -i[1]
+    for i in dots:
+        print_dots(i[0] + xx0, i[1] + yy0)
+    print_dots(xx0, yy0)
+    print_dots(xx1, yy1)
+    # deltax = abs(x1 - x0)
+    # deltay = abs(y1 - y0)
+    # if deltax > deltay:
+    #
+    #     deltaerr = (deltay + 1)
+    #     y = y0
+    #     diry = y1 - y0
+    #     if diry > 0:
+    #         diry = 1
+    #     if diry < 0:
+    #         diry = -1
+    #     if x0 < x1:
+    #         for x in range(x0, x1):
+    #             print_pixel(x, y)
+    #             error += deltaerr
+    #             if error >= (deltax + 1):
+    #                 y += diry
+    #                 error -= (deltax + 1)
+    #     else:
+    #         for x in range(x0, x1, -1):
+    #             print_pixel(x, y)
+    #             error += deltaerr
+    #             if error >= (deltax + 1):
+    #                 y += diry
+    #                 error -= (deltax + 1)
+    # else:
+    #     error = 0
+    #     deltaerr = (deltax + 1)
+    #     x = x0
+    #     dirx = x1 - x0
+    #     if dirx > 0:
+    #         dirx = 1
+    #     if dirx < 0:
+    #         dirx = -1
+    #     if y0 < y1:
+    #         for y in range(y0, y1):
+    #             print_pixel(x, y)
+    #             error += deltaerr
+    #             if error >= (deltay + 1):
+    #                 x += dirx
+    #                 error -= (deltay + 1)
+    #     else:
+    #         for y in range(y0, y1, -1):
+    #             print_pixel(x, y)
+    #             error += deltaerr
+    #             if error >= (deltay + 1):
+    #                 x += dirx
+    #                 error -= (deltay + 1)
 
 
 def print_pixel(x1, y1):
@@ -96,9 +145,7 @@ def draw_circle(x0, y0, radius):
         y -= 1
 
 
-
-
-xx1, yy1, xx2, yy2 = 4, 4, 6, 5
+xx1, yy1, xx2, yy2 = 4, 4, 7, -4
 oxx, oyy, rr = 2, 2, 1
 x1, y1, x2, y2 = f_coord(xx1), f_coord(yy1), f_coord(xx2), f_coord(yy2)
 win = tk.Tk()
@@ -110,7 +157,7 @@ canvas = tk.Canvas(win, bg="white", width=800, height=800)
 canvas.place(x=0, y=0)
 draw_grid()
 # canvas.create_line(x1 + 100, y1 + 100, x2 + 100, y2 + 100)
-# line(x1, y1, x2, y2)
+line(x1, y1, x2, y2)
 # canvas.create_oval(ox - r + 200, oy - r, ox + r + 200, oy + r, outline='red')
 # draw_circle(ox, oy, r)
 win.mainloop()
