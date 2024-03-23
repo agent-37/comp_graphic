6f3a5162cfa224df59eb907b1f9ef7858fb60416
Иван Маслеников <bdfy.maslenikov@yandex.ru>
2024-03-23 16:48:32 +0300

    lab 4a

--- lab_4_a.py
+++ lab_4_a.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 from cmath import cos, sin, pi, sqrt, acos, asin
+from math import ceil
 
 mashtab = 20
+
+
+def print_dots(x, y, color):
+    global canvas
+    x1 = x
+    y1 = 800 - y
+    canvas.create_oval(x1 - 3, y1 - 3, x1 + 3, y1 + 3, fill=color)
 
 
 def f_coord(x):
@@ -83,7 +91,7 @@ if (find_mask(ax, ay) == 0 and find_mask(bx, by) != 0) or (find_mask(bx, by) == 
     flag = -1
 
 win = tk.Tk()
-win.title("lab 3")
+win.title("lab 4")
 win.geometry("800x800")
 win.resizable(False, False)
 # ------------------------------------------------------------
@@ -93,10 +101,40 @@ canvas.place(x=0, y=0)
 draw_grid()
 
 canvas.create_rectangle(f_coord(l), 800 - f_coord(d), f_coord(r), 800 - f_coord(u), outline='red')
-canvas.create_line(f_coord(ax), 800 - f_coord(ay), f_coord(bx), 800 - f_coord(by))
+canvas.create_line(f_coord(ax), 800 - f_coord(ay), f_coord(bx), 800 - f_coord(by), fill='blue')
+t = set()
+
+
+def check_pere(P1, P2, A, B):
+    global t
+    P2P1 = [P1[0] - P2[0], P1[1] - P2[1]]
+    N = [-P2P1[1], P2P1[0]]
+    if (N[0] * (B[0] - A[0]) + N[1] * (B[1] - A[1])) == 0:
+        return
+    tt = (N[0] * P1[0] - A[0] * N[0] + N[1] * P1[1] - N[1] * A[1]) / (N[0] * (B[0] - A[0]) + N[1] * (B[1] - A[1]))
+    if tt > 1 or tt < 0:
+        return
+    if min(P1[0], P2[0]) <= A[0] + tt * (B[0] - A[0]) <= max(P1[0], P2[0]) and min(P1[1], P2[1]) <= A[1] + tt * (
+            B[1] - A[1]) <= max(P1[1], P2[1]):
+        t.add(tt)
+
+
 if abs(flag):
     if flag == 1:
-        t = []
-        for i in range(n-1):
-
+        for i in range(n - 1):
+            check_pere(dots[i], dots[i + 1], A, B)
+        check_pere(dots[n - 1], dots[0], A, B)
+        pr_dots = []
+        for i in t:
+            pr_dots.append([ceil(f_coord((1 - i) * A[0] + i * B[0])), ceil(f_coord((1 - i) * A[1] + i * B[1]))])
+        for i in pr_dots:
+            print_dots(i[0], i[1], 'green')
+        # print(pr_dots)
+        if len(pr_dots) >= 2:
+            canvas.create_line(pr_dots[0][0], 800 - pr_dots[0][1], pr_dots[1][0], 800 - pr_dots[1][1], fill='orange')
+    else:
+        for i in range(n - 1):
+            check_pere(dots[i], dots[i + 1], A, B)
+        check_pere(dots[n - 1], dots[0], A, B)
+        print(t)
 win.mainloop()
